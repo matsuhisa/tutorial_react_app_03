cd355d5575ee9b6c978941e85fb85f29fa1be178
matsuhisa <matsuhisa+github@gmail.com>
2020-03-23 00:41:00 +0900

    Use useState for counter
    - Hook を使ってみた例

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App: FC = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount( count + 1)
+  }
+
+  const decrement = () => {
+    setCount( count - 1 )
+  }
+
+  return(
+    <div>
+      <h1>カウンター</h1>
+      <h2>count：{count}</h2>
+      <div onClick={decrement} > マイナス </div>
+      <div onClick={increment} > 追加 </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
